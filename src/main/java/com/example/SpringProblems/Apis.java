09bc76b6e4145a9405b626e822631bd5f1f33755
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-22 01:17:10 +0530

    [Nishant]Add.useCase3

--- src/main/java/com/example/SpringProblems/Apis.java
+++ src/main/java/com/example/SpringProblems/Apis.java
@@ -1,6 +1,7 @@
 package com.example.SpringProblems;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,17 @@ public class Apis {
     public String hello(){
         return "Hello User";
     }
-    @GetMapping("/greet")
+    @GetMapping("/greet")// get the response according to the query parameter
     public String getUser(@RequestParam(name="name")String name){
       {
             return "Hello" + name;
         }
     }
+    @GetMapping("/hello/{params}")
+    public String getUserbyparams(@PathVariable String params){
+        return "Hello "+params;
+    }
+
+
+
 }
