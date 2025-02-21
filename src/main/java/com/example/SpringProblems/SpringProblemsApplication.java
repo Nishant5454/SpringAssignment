23298ff9187282cb92719cb87985452361cf7475
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-21 16:05:09 +0530

    init

---
+++ src/main/java/com/example/SpringProblems/SpringProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringProblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProblemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringProblems/SpringProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringProblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
