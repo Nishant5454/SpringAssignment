dcd43b4c28a2f747584e85f6459c0a86e7fe7941
Nishant5454 <bhagatnishant5454@gmail.com>
2025-02-21 16:52:49 +0530

    [Nishant]Add.Get User using Params

--- src/main/java/com/example/SpringProblems/Apis.java
+++ src/main/java/com/example/SpringProblems/Apis.java
@@ -1,12 +1,19 @@
 package com.example.SpringProblems;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GetApi {
+public class Apis {
     @GetMapping("/hello")// get the response from server
     public String hello(){
         return "Hello User";
+    }
+    @GetMapping("/greet")
+    public String getUser(@RequestParam(name="name")String name){
+      {
+            return "Hello" + name;
+        }
     }
 }
